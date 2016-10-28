09c322233aeca762aacc9c269944c28b1ba70354
Steve Schmitt <steves@ideablade.com>
2016-10-27 17:41:32 -0700

    Bundling with rollup.  Type `npm run rollup` to try it.

--- rollup.js
+++ rollup.js
@@ -1,47 +1,19 @@
 import rollup      from 'rollup';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs    from 'rollup-plugin-commonjs';
-import uglify      from 'rollup-plugin-uglify';
-import replace     from 'rollup-plugin-replace';
-import alias       from 'rollup-plugin-strict-alias';
-import path        from 'path';
+import multiEntry  from 'rollup-plugin-multi-entry';
 
 export default {
-  entry: 'app/main.js',
-  dest: 'dist/build.js', // output a single application bundle
+  entry: [
+        './src/entity-manager.js',
+        './src/relation-array.js',
+        './src/primitive-array.js',
+        './src/complex-array.js'
+      ],
+
+  dest: 'build/breeze.es2015.js', // must be transpiled after
+  moduleName: 'breeze',
   sourceMap: true,
   format: 'iife',
   plugins: [
-      alias({
-          'breeze-client': path.resolve(__dirname, 'node_modules', 'breeze-client/breeze.base.debug.js')
-      }),
-      nodeResolve({jsnext: true, module: true}),
-      replace({
-          'moduleId: module.id,': ''
-      }),
-      commonjs({
-        include: [
-            'node_modules/rxjs/**', 
-            'node_modules/breeze-client/**', 
-            'node_modules/breeze-bridge-angular2/**', 
-            'node_modules/lodash/**'
-        ],
-        exclude: [
-            'node_modules/breeze-client/breeze.debug.js'
-        ],
-        namedExports: {
-            'node_modules/breeze-client/breeze.base.debug.js': [
-                'core',
-                'config',
-                'NamingConvention',
-                'DataService',
-                'EntityManager',
-                'EntityQuery',
-                'FetchStrategy',
-                'SaveOptions'
-            ]
-        }
-      }),
-      uglify()
+      multiEntry()
   ]
 }
