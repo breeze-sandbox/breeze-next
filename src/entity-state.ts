70cb5c3186d4e22febc1ca7786d6ebf72de9ecf8
Jay Traband <jtraband@comcast.net>
2016-11-15 16:29:00 -0800

    more tsdoc...

--- src/entity-state.ts
+++ src/entity-state.ts
@@ -2,118 +2,100 @@
 
 /**
 EntityState is an 'Enum' containing all of the valid states for an 'Entity'.
-
 **/
 export class EntityState extends BreezeEnum {
 
-  /**
-  The 'Unchanged' state.
-  **/
+  /** The 'Unchanged' state. **/
   static Unchanged = new EntityState();
-  /**
-  The 'Added' state.
-  **/
+  /**  The 'Added' state.  **/
   static Added = new EntityState();
-  /**
-  The 'Modified' state.
-  **/
+  /**  The 'Modified' state.   **/
   static Modified = new EntityState();
-  /**
-  The 'Deleted' state.
-  **/
+  /**  The 'Deleted' state.  **/
   static Deleted = new EntityState();
-  /**
-  The 'Detached' state.
-  **/
+  /**  The 'Detached' state.  **/
   static Detached = new EntityState();
 
- /**
-  @example
-      var es = anEntity.entityAspect.entityState;
-      return es.isUnchanged();
+  /**
+  Returns whether an entityState instance is EntityState.Unchanged.
+  >     var es = anEntity.entityAspect.entityState;
+  >     return es.isUnchanged();
+
   is the same as
-  @example
-      return es === EntityState.Unchanged;
-  @method isUnchanged
-  @return {Boolean} Whether an entityState instance is EntityState.Unchanged.
+  >     return es === EntityState.Unchanged;
   **/
   isUnchanged() {
     return this === EntityState.Unchanged;
   }
+
   /**
-  @example
-      var es = anEntity.entityAspect.entityState;
-      return es.isAdded();
+  Return whether an entityState instance is EntityState.Added.
+  
+  >     var es = anEntity.entityAspect.entityState;
+  >     return es.isAdded();
+
   is the same as
-  @example
-      return es === EntityState.Added;
-  @method isAdded
-  @return {Boolean} Whether an entityState instance is EntityState.Added.
+  >     return es === EntityState.Added;
   **/
   isAdded() {
     return this === EntityState.Added;
   }
+
   /**
-  @example
-      var es = anEntity.entityAspect.entityState;
-      return es.isModified();
+  Returns whether an entityState instance is EntityState.Modified.
+  >     var es = anEntity.entityAspect.entityState;
+  >     return es.isModified();
+
   is the same as
-  @example
-      return es === EntityState.Modified;
-  @method isModified
-  @return {Boolean} Whether an entityState instance is EntityState.Modified.
+  >     return es === EntityState.Modified;
   **/
   isModified() {
     return this === EntityState.Modified;
   }
+
   /**
-  @example
-      var es = anEntity.entityAspect.entityState;
-      return es.isDeleted();
+  Returns whether an entityState instance is EntityState.Deleted.
+  >     var es = anEntity.entityAspect.entityState;
+  >     return es.isDeleted();
+
   is the same as
-  @example
-      return es === EntityState.Deleted;
-  @method isDeleted
-  @return  {Boolean} Whether an entityState instance is EntityState.Deleted.
+  
+  >     return es === EntityState.Deleted;
   **/
   isDeleted() {
     return this === EntityState.Deleted;
   }
+
   /**
-  @example
-      var es = anEntity.entityAspect.entityState;
-      return es.isDetached();
+  Returns whether an entityState instance is EntityState.Detached.
+  >     var es = anEntity.entityAspect.entityState;
+  >     return es.isDetached();
+
   is the same as
-  @example
-      return es === EntityState.Detached;
-  @method isDetached
-  @return  {Boolean} Whether an entityState instance is EntityState.Detached.
+  >     return es === EntityState.Detached;
   **/
   isDetached() {
     return this === EntityState.Detached;
   }
+
   /**
-  @example
-      var es = anEntity.entityAspect.entityState;
-      return es.isUnchangedOrModified();
+  Returns whether an entityState instance is EntityState.Unchanged or EntityState.Modified.
+  >     var es = anEntity.entityAspect.entityState;
+  >     return es.isUnchangedOrModified();
+
   is the same as
-  @example
-      return es === EntityState.Unchanged || es === EntityState.Modified
-  @method isUnchangedOrModified
-  @return {Boolean} Whether an entityState instance is EntityState.Unchanged or EntityState.Modified.
+  >     return es === EntityState.Unchanged || es === EntityState.Modified
   **/
   isUnchangedOrModified() {
     return this === EntityState.Unchanged || this === EntityState.Modified;
   }
-  /**
-  @example
-      var es = anEntity.entityAspect.entityState;
-      return es.isAddedModifiedOrDeleted();
+
+  /** Returns whether an entityState instance is EntityState.Added or EntityState.Modified or EntityState.Deleted.
+  >     var es = anEntity.entityAspect.entityState;
+  >     return es.isAddedModifiedOrDeleted();
+
   is the same as
-  @example
-      return es === EntityState.Added || es === EntityState.Modified || es === EntityState.Deleted
-  @method isAddedModifiedOrDeleted
-  @return {Boolean} Whether an entityState instance is EntityState.Added or EntityState.Modified or EntityState.Deleted.
+  >     return es === EntityState.Added || es === EntityState.Modified || es === EntityState.Deleted
   **/
   isAddedModifiedOrDeleted() {
     return this === EntityState.Added ||
@@ -124,5 +106,3 @@ export class EntityState extends BreezeEnum {
 }
 
 EntityState.resolveSymbols();
-
-
