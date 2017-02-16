f0e8ac80f83dc53238501bbeeaa154a6640c74a4
Steve Schmitt <steves@ideablade.com>
2017-02-15 22:48:15 -0800

    Only copy changed files to the docs folder

--- readme.doc.md
+++ readme.doc.md
@@ -2,7 +2,7 @@
 Next version of Breeze, built using TypeScript 2.
 
 ## Breaking changes
-Api is almost identical to the original but small changes are noted below:
+API is almost identical to the original, but small changes are noted below:
 
 Breeze no longer depends upon Q.js.  But it does depend on a ES6 promise implementation. i.e. the existence of a global Promise object. 
 
--- rollup/copyIfDifferent.js
+++ rollup/copyIfDifferent.js
@@ -1,13 +1,13 @@
 /** 
- * Copy file but only if content is different
+ * Copy file/dirctory but only if content is different
  */
 var fs = require("fs-extra");
 var isDifferent = require("./build-util").isDifferent;
 
 var args = process.argv.slice(2);
 if (args.length < 2) {
-  console.log("Usage: " + process.argv[0] + " " + process.argv[1] + " [srcfile] [destfile]\n" + 
-  "Copies srcfile to destfile, if content of src is different from dest.");
+  console.log("Usage: " + process.argv[0] + " " + process.argv[1] + " [src] [dest]\n" + 
+  "Copies src (file or directory) to dest, if content of src is different from dest.");
   return;
 }
 
@@ -18,9 +18,16 @@ if (!fs.existsSync(srcName)) {
   console.error("Source file '" + srcName + "' does not exist.");
   return;
 }
+var srcLen = srcName.length;
 
-if (isDifferent(srcName, destName)) {
-  fs.copySync(srcName, destName);
-}
+fs.copySync(srcName, destName, { filter: filter });
 return;
+
+// return true if file should be copied, false otherwise
+function filter(src) {
+  var stats = fs.statSync(src);
+  if (!stats.isFile()) return true;
+  var dest = destName + src.substring(srcLen);
+  return isDifferent(src, dest);
+}
 
